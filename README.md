9b58af9e25f6a8aa18c3b216274b04deed38abba
Brendan Bischoff <brendanbischoff@gmail.com>
2022-02-03 20:31:43 -0500

    finished index.js file

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# README-generator
-This is a simple README.md generator application that captures user input via inquirer npm and creates a professional grade file for the users needs. Dependencies include Javascript, HTML,  Node.js, Inquirer
+# README
+
--- index.js
+++ index.js
@@ -1,15 +1,147 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+// Require Modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// README Generate Page
+const generatePage = require('./utils/generateMarkdown.js');
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Questions Array For User
+const questions = () => {
+    // Prompting Questions With Inquirer
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false; 
+            }
+        }
 
-// TODO: Create a function to initialize app
-function init() {}
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your project name!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of your project!');
+                return false; 
+            }
+        }
+    }, 
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU'],
+        default: ["MIT"],
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please choose a license!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps required to install your project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter steps required to install your project!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this app?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a usage description!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test', 
+        message: 'What command should be run to run tests?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'What does the user need to know about contributing to the repo?'
+    }
+]);
+};
 
-// Function call to initialize app
-init();
+// Function To Write README File
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        // IF There Is An Error
+        if (err) {
+            console.log(err);
+            return;
+        // When The README has been successfully created
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+}; 
+
+// Function Call To Initialize Program
+questions()
+// Collecting User Answers
+.then(answers => {
+    return generatePage(answers);
+})
+// Using Data To Display On Page
+.then(data => {
+    return writeFile(data);
+})
+// Catching Errors
+.catch(err => {
+    console.log(err)
+})
